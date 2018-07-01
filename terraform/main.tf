b1a3b559c08557f210c3dd532918ef10caf5fcbb
Alexander Kryuchkov <kruchkov.alexandr@gmail.com>
2018-07-01 23:33:45 +0300

    начало работы над ДЗ с терраформом

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+version = "1.4.0"
+project = "top-alliance-208117"
+region = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+name = "reddit-app"
+machine_type = "g1-small"
+zone = "europe-west1-b"
+# определение загрузочного диска
+boot_disk {
+initialize_params {
+image = "reddit-base"
+}
+}
+# определение сетевого интерфейса
+network_interface {
+# сеть, к которой присоединить данный интерфейс
+network = "default"
+# использовать ephemeral IP для доступа из Интернет
+access_config {}
+}
+}
