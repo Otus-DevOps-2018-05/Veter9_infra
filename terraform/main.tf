fd25d984fc3239f0a52bf4b05f986bd6d045395c
Alexander Kryuchkov <kruchkov.alexandr@gmail.com>
2018-07-08 13:52:25 +0300

    Выполнение домашнего задания до слайда 48.
    Так сказать коммит перед самостоятельным заданием.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,37 @@
 provider "google" {
 version = "1.4.0"
-project = "top-alliance-208117"
-region = "europe-west1"
+project = "${var.project}"
+region = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
 name = "reddit-app"
 machine_type = "g1-small"
 zone = "europe-west1-b"
+  tags = ["reddit-app"]
+
+  connection {
+    type = "ssh"
+    user = "alexander"
+    agent = "false"
+    private_key = "${file("~/.ssh/google_app")}"
+  }
+
+provisioner "file" {
+  source = "files/puma.service"
+  destination = "/tmp/puma.service"
+}
+provisioner "remote-exec" {
+    script = "files/deploy.sh"
+}
+
+metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
 # определение загрузочного диска
 boot_disk {
 initialize_params {
-image = "reddit-base"
+image = "${var.disk_image}"
 }
 }
 # определение сетевого интерфейса
@@ -21,4 +41,18 @@ network = "default"
 # использовать ephemeral IP для доступа из Интернет
 access_config {}
 }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = [
+      "9292"]
+  }
+  source_ranges = [
+    "0.0.0.0/0"]
+  target_tags = [
+    "reddit-app"]
 }
