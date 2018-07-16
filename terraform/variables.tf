8cf689712e3117454281c8ca03651fab3044a7f5
Alexander Kryuchkov <kruchkov.alexandr@gmail.com>
2018-07-16 22:52:12 +0300

    Перегенерил ключ ssh

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable project {
+  description = "Project ID"
+  default     = "top-alliance-208117"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Path to the private key for ssh access"
+}
+
+variable zone {
+  description = "default zone"
+  default     = "europe-west1-b"
+}
+
+variable "app_disk_image" {
+  description = "Disk image fro Reddit app"
+  default     = "reddit-app-base"
+}
+
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
